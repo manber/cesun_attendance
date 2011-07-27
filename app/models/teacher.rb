891a09a0ff91140c561e07edfb733b2c748560fc
manuel <bernalgas@gmail.com>
2011-07-26 21:08:31 -0700

    added relations to assignments and checkins

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,7 @@
 class Teacher < ActiveRecord::Base
   belongs_to :user
+  has_many :assignments
+  has_many :checkins
   validates_uniqueness_of :control_number
   validates_numericality_of :control_number
   validates_presence_of :name, :paternal_surname, :user
